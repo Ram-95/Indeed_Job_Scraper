1ce277851ca186e5c3cc39f92ea2c73f13568d45
Ram Babu <ram.kottapally@gmail.com>
2019-10-27 17:51:31 +0530

    Add files via upload

--- Indeed_Job_scraper.py
+++ Indeed_Job_scraper.py
@@ -1,6 +1,7 @@
 #Indeed Job Scraper
 ''' Given the Skill and the Location, this script will scrape the Indeed Indian website and present the matching job details.'''
 
+import csv
 import requests
 import bs4 as bs
 from prettytable import PrettyTable
@@ -11,6 +12,12 @@ headers = {"User-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHT
 skill = input('Enter your Skill: ').strip()
 place = input('Enter the location: ').strip()
 
+#Name of the CSV File
+file_name = skill.title() + '_' + place.title() + '_Jobs.csv'
+#Path of the CSV File
+file_path = 'C:\\Users\\shyam\\Desktop\\' + file_name
+
+#Requesting and getting the webpage using requests
 url = 'https://www.indeed.co.in/jobs?q=' + skill + '&l=' + place
 response = requests.get(url, headers = headers)
 html = response.text
@@ -19,26 +26,35 @@ html = response.text
 soup = bs.BeautifulSoup(html, 'lxml')
 jobs = soup.findAll('div', class_ = 'jobsearch-SerpJobCard')
 
+#Writing to the CSV File
+with open(file_path, mode = 'w') as file:
+    writer = csv.writer(file, delimiter = ',', lineterminator = '\n')
 
-#Table to present the Job Details
-table = PrettyTable(['Job_Name', 'Company', 'Location'])
+    #Adding the Column Names to the CSV File
+    writer.writerow(['Job_Name', 'Company', 'Location'])
 
-#Getting the Job Details
-for i in jobs:
-    #Getting the Job Names
-    job_name = i.find('div', class_ = 'title').find('a')['title'].strip('\n')
+    #Table to present the Job Details
+    table = PrettyTable(['Job_Name', 'Company', 'Location'])
 
-    #Getting the Company Names
-    company = i.find('div', class_ = 'sjcl').find('span', class_ = 'company').text.strip('\n').title()
+    #Getting the Job Details
+    for i in jobs:
+        #Getting the Job Names
+        job_name = i.find('div', class_ = 'title').find('a')['title'].strip('\n').title()
 
-    #Getting the Locations
-    if i.find('div', class_ = 'sjcl').find('div', class_ = 'location') is None:
-        location = i.find('div', class_ = 'sjcl').find('span', class_ = 'location').text.strip('\n')
-    else:
-        location = i.find('div', class_ = 'sjcl').find('div', class_ = 'location').text.strip('\n')
+        #Getting the Company Names
+        company = i.find('div', class_ = 'sjcl').find('span', class_ = 'company').text.strip('\n').title()
 
-    table.add_row([job_name, company, location])
+        #Getting the Locations
+        if i.find('div', class_ = 'sjcl').find('div', class_ = 'location') is None:
+            location = i.find('div', class_ = 'sjcl').find('span', class_ = 'location').text.strip('\n')
+        else:
+            location = i.find('div', class_ = 'sjcl').find('div', class_ = 'location').text.strip('\n')
 
+        #Adding to the table row
+        table.add_row([job_name, company, location.title()])
+
+        #Writing to CSV File
+        writer.writerow([job_name, company, location.title()])
     
 #Printing the final Table
 print('\n********** Job Details for \'{}\' at \'{}\' ***********'.format(skill.title(), place.title()))
